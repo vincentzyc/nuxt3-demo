44122fea2de2a4f2a2707612cb656a76e3570c32
vincentzyc <vincentzyc@qq.com>
2023-07-26 09:51:35 +0800

    🎉 init: init

---
+++ components/FormIDCard/FormIDCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex align-middle form-item">
+    <div class="form-label">身份证号</div>
+    <div class="form-input flex-auto flex align-middle">
+      <input
+        @blur="checkIDCard(model)"
+        :maxlength="18"
+        placeholder="输入办理人身份证（已加密）"
+        type="text"
+        v-model.trim="model"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { checkIDCard } from '~/utils/business/verify-data';
+import { TypeModelProps, TypeModelEmits, useModelValue } from '~/composition/use-model';
+
+const props = defineProps<TypeModelProps>();
+
+const emits = defineEmits<TypeModelEmits>();
+
+const { model } = useModelValue(props, emits);
+</script>
