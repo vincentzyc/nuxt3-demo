288a4a6f34471ca12a0b6aa2d37d19465ef26c15
vincentzyc <vincentzyc@qq.com>
2023-07-28 17:15:35 +0800

    🎉 init: init

---
+++ components/FormPhone/FormPhone.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex align-middle form-item">
+    <div class="form-label">联系电话</div>
+    <div class="form-input flex-auto flex align-middle">
+      <input
+        @blur="checkPhone(model)"
+        :maxlength="11"
+        placeholder="输入有效联系电话并保持畅通"
+        type="tel"
+        v-model.trim="model"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { checkPhone } from '~/utils/business/verify-data';
+import { TypeModelProps, TypeModelEmits, useModelValue } from '~/composition/use-model';
+
+const props = defineProps<TypeModelProps>();
+
+const emits = defineEmits<TypeModelEmits>();
+
+const { model } = useModelValue(props, emits);
+</script>
