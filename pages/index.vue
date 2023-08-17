af1112d1c067a797241e89bd37bb25a3066dc4c7
vincentzyc <vincentzyc@qq.com>
2023-08-17 09:43:22 +0800

    🎉 init: init

--- pages/index.vue
+++ pages/index.vue
@@ -24,6 +24,7 @@
 <script setup lang="ts">
 import { expensesText } from '~/assets/js/static-data';
 import img3 from '~/assets/img/home/img3.jpg';
+import { CommonApi } from '@/api';
 import { useMainStore } from '@/pinia';
 
 // const { data } = await useFetch('/api/hello');
@@ -51,12 +52,19 @@ useHead({
   // script: [ { children: 'console.log(\'Hello world\')' } ]
 });
 
+async function getBaseData(pid: string, href: string) {
+  const mainStore = useMainStore();
+  const param = { pid: pid, url: href };
+  let res = await CommonApi.pageIdLocation(param);
+  mainStore.setCjData(res);
+}
+
 function setPgaePid() {
   const url = useRequestURL();
   const searchParams = new URLSearchParams(url.searchParams);
   const params = Object.fromEntries(searchParams.entries());
   mainStore.setUrlParams(params);
-  // getBaseData();
+  if (mainStore.urlParams.pid) getBaseData(mainStore.urlParams.pid, url.href);
 }
 setPgaePid();
 </script>
